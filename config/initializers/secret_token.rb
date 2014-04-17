7103d855ac3e6aed2feea766e24cdffd90f54703
Justin Bogan <jbogan@mullen.com>
2014-04-17 11:13:54 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MullenMaps::Application.config.secret_token = '1f5cd8a8a63bdb9b4634e58feb89e6deb01c8fd53de3985ce7994e8d8ae4231a216d673243c435a6683975bd0471121abaf51a38902c5b629c85a1fda846efd5'
