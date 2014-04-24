be903695b647c81b937155581bcc8f7ffc9def13
Justin Bogan <jbogan@mullen.com>
2014-04-24 16:31:58 -0400

    update and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,26 @@ class UsersController < ApplicationController
   	@user = User.new
   end
   
+  def index
+  	@users = User.all
+  end
+
   def show
   	@user = User.find(params[:id])
+  end
+  
+  def edit
+  	@user = User.find(params[:id])
+  end
+  
+  def update
+  	@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Info updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -23,4 +23,6 @@ ActiveRecord::Schema.define(version: 20140423174622) do
     t.datetime "updated_at"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+
 end
