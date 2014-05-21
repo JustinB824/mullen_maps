58736854174c7773904b85f87ba475b3ca832bde
Justin Bogan <jbogan@mullen.com>
2014-05-21 09:00:16 -0400

    client-side validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@ var map;
 var addressInput;
 var autocomplete;
 var newAddress;
+var emailRegEx = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 var storedAddresses = new Array();
 
 $(function() {
@@ -38,11 +39,46 @@ $(function() {
 		CheckValues();
 	});
 	
-	$('#searchTextField').blur(function() {
-		//alert('stop!');
-		//if (!$('#user_address_coords').val()) {
-			//$('#searchTextField').val('');
-		//}
+	$('.users_form form').on('submit', function(e) {
+		if (($('#user_first_name').hasClass('emptyVal')) || ($('#user_first_name').val().trim() == '')) {
+			$('#fn_error').html('You must enter your First Name');
+			e.preventDefault();
+		}
+		else {
+			$('#fn_error').html('');
+		}
+		
+		if (($('#user_last_name').hasClass('emptyVal')) || ($('#user_last_name').val().trim() == '')) {
+			$('#ln_error').html('You must enter your Last Name');
+			e.preventDefault();
+		}
+		else {
+			$('#ln_error').html('');
+		}
+
+		if (($('#user_email').hasClass('emptyVal')) || ($('#user_email').val().trim() == '')) {
+			$('#email_error').html('You must enter your Email Address');
+			e.preventDefault();
+		}
+		else if (($('#user_email').val().trim() != '') && (!$('#user_email').hasClass('emptyVal')) && (!emailRegEx.test($('#user_email').val()))) {
+			$('#email_error').html('Please enter a valid Email Address');
+			e.preventDefault();
+		}
+		else {
+			$('#email_error').html('');
+		}
+		
+		if (($('#searchTextField').hasClass('emptyVal')) || ($('#searchTextField').val().trim() == '')) {
+			$('#address_error').html('You must enter your Address or City');
+			e.preventDefault();
+		}
+		else if (($('#searchTextField').val().trim() != '') && ($('#user_address_coords').val() == '')) {
+			$('#address_error').html('You must select your location from the MapQuest dropdown options');
+			e.preventDefault();
+		}
+		else {
+			$('#address_error').html('');
+		}
 	});
 	
 	$(window).keydown(function(event){
